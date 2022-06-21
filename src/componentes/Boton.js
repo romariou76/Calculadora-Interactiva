8f761df6056e270451299bc7b342a1300744333d
romariou76 <illeniumr@gmail.com>
2022-06-21 18:22:39 -0500

    Definimos estilos para los botones y creamos los botones asi como los operadores

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,4 +1,5 @@
 import react from 'react';
+import '../hojas-de-estilo/Boton.css';
 
 function Boton(props) {
 
@@ -7,7 +8,7 @@ function Boton(props) {
     };                                                             //trimEnd para ocultar espacios
     return (
         <div
-            className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}>
+            className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}>{/*Aqui definimos 2 clases , boton-contnedor y operador */}
             {props.children}
         </div>
     )
